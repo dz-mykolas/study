7c77d471fa7ef02299a1eb1757df8dcee56e2610
Mykolas Dzidzevičius <108366398+dz-mykolas@users.noreply.github.com>
2023-02-01 12:35:17 +0200

    Add files via upload

--- teltonika/part3_task1_address_book/llist.c
+++ teltonika/part3_task1_address_book/llist.c
@@ -3,14 +3,45 @@
 
 #include "llist.h"
 
-struct Person *createNode()
-
-void llist_add_end(struct Person **list, char *name, char *surname, char *email, char *phone)
+struct Person *createNode(char *name, char *surname, char *email, char *phone)
 {
     
 }
 
-void llist_add_at(struct Person **list, char *name, char *surname, char *email, char *phone)
+void llist_add_end(struct Person **list, struct Person *p)
 {
-    
+    // check head
+    struct Person *tmp = *list;
+    if (tmp == NULL){
+        *list = p;
+        return;
+    }
+
+    while (tmp->next != NULL){
+        tmp = tmp->next;
+    }
+    tmp->next = p;
+}
+
+int llist_add_at(struct Person **list, struct Person *p, int pos)
+{
+    // check head
+    struct Person *tmp = *list;
+    if (tmp == NULL){
+        if (pos != 0)
+            return 1;
+        *list = p;
+        return 0;
+    }
+
+    int i = 0;
+    while (tmp->next != NULL && i < pos){
+        previous = tmp;
+        tmp = tmp->next;
+        i++;
+    }
+    if (pos > i)
+        return 1;
+    previous->next = p;
+    p->next = tmp;
 }
